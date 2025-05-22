31234b91d6aed7bd0dbdfb094eae5dc0b01e1066
Rodrigo Aguiar <86214343+RodrigoAguiarS@users.noreply.github.com>
2025-05-22 04:17:08 -0300

    style: remover métodos formParaEntidade dos mappers

--- src/main/java/br/com/rodrigo/api/controleestoque/conversor/IMapper.java
+++ src/main/java/br/com/rodrigo/api/controleestoque/conversor/IMapper.java
@@ -2,6 +2,5 @@ package br.com.rodrigo.api.controleestoque.conversor;
 
 public interface IMapper<E, F, R> {
     R entidadeParaResponse(E entidade);
-    E formParaEntidade(F form);
     E responseParaEntidade(R response);
 }
